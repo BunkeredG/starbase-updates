618bb7ab9ed3ffc434f6d42f1b6d99063e1a480c
BunkeredG <leomagaro@icloud.com>
2026-09-26 10:50:25 -0500

    Scrape fix
    Scrape launches now only searches the correct part of the page and removes windows that are closed

--- scrape_launches.py
+++ scrape_launches.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import json
+from datetime import datetime, timezone
 from bs4 import BeautifulSoup
 
 # WORD##
@@ -11,11 +12,13 @@ url = "https://www.fly.faa.gov/adv/adv_spt"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 text_orig = soup.get_text()
+purged = re.search(r'REENTRY:(.*)FLIGHT CHECK', text_orig, re.DOTALL)
+launch_text = purged.group(1)
 
 def formatDate(month, day, hour, min):
     return f"2026-{month}-{day}T{hour}:{min}:00Z"
 
-textL = text_orig.split("\n")
+textL = launch_text.split("\n")
 carousel_data = []
 for i, line in enumerate(textL):
     if ("SPACEX" in line or "TRANSPORTER" in line) and "REENTRY" not in line:
@@ -49,13 +52,16 @@ for i, line in enumerate(textL):
         window_open = text_split[2][0:4]
         window_close = text_split[2][6:10]
 
+        current_time = datetime.now(timezone.utc).strftime('%m%d%H%M')
+        close_time = date_split[0] + date_split[1] + str(window_close[0:4])
+        
+        if close_time > current_time:
+            open_z = formatDate(date_split[0], date_split[1], window_open[0:2], window_open[2:4])
+            close_z = formatDate(date_split[0], date_split[1], window_close[0:2], window_close[2:4])
 
-        open_z = formatDate(date_split[0], date_split[1], window_open[0:2], window_open[2:4])
-        close_z = formatDate(date_split[0], date_split[1], window_close[0:2], window_close[2:4])
-
-        print(mission, open_z, close_z, sep='\n')
-        carousel_data.append([open_z, close_z, mission])
-        print("\n")
+            print(mission, open_z, close_z, sep='\n')
+            carousel_data.append([open_z, close_z, mission])
+            print("\n")
 
 print(carousel_data)
 with open('launches.json', 'w') as f:
